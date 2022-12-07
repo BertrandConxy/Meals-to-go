103fde17a424c467644dbdc98093bec738348777
Bertrand <mutanganabertrand@gmail.com>
2022-12-07 16:08:13 +0200

    Add indicator in the Login page

--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from 'react'
+import { ActivityIndicator, MD2Colors } from 'react-native-paper'
 import AccountBackground, {
   AccountContainer,
   ErrorContainer,
@@ -10,43 +11,49 @@ import { Text } from '../../../components/typography/text.component'
 import { AuthenticationContext } from '../../../services/authentication/authentication.context'
 
 const LoginScreen = ({ navigation }) => {
-  const { onLogin, error } = useContext(AuthenticationContext)
+  const { onLogin, error, isLoading } = useContext(AuthenticationContext)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   return (
     <AccountBackground>
       <AccountContainer>
-        <AuthInput
-          label="Email"
-          value={email}
-          textContentType="emailAddress"
-          keyboardType="email-address"
-          autoCapitalize="none"
-          onChangeText={(u) => setEmail(u)}
-        />
-        <Spacer size="large" />
-        <AuthInput
-          label="Password"
-          value={password}
-          textContentType="password"
-          secureTextEntry
-          autoCapitalize="none"
-          onChangeText={(p) => setPassword(p)}
-        />
-        <Spacer size="large" />
-        <AuthButton
-          icon="lock-open-outline"
-          mode="contained"
-          onPress={() => {
-            onLogin(email, password)
-          }}
-        >
-          Login
-        </AuthButton>
-        {error && (
-          <ErrorContainer>
-            <Text variant="error">Error: {error}</Text>
-          </ErrorContainer>
+        {isLoading ? (
+          <ActivityIndicator animating={true} color={MD2Colors.blue500} />
+        ) : (
+          <>
+            <AuthInput
+              label="Email"
+              value={email}
+              textContentType="emailAddress"
+              keyboardType="email-address"
+              autoCapitalize="none"
+              onChangeText={(u) => setEmail(u)}
+            />
+            <Spacer size="large" />
+            <AuthInput
+              label="Password"
+              value={password}
+              textContentType="password"
+              secureTextEntry
+              autoCapitalize="none"
+              onChangeText={(p) => setPassword(p)}
+            />
+            <Spacer size="large" />
+            <AuthButton
+              icon="lock-open-outline"
+              mode="contained"
+              onPress={() => {
+                onLogin(email, password)
+              }}
+            >
+              Login
+            </AuthButton>
+            {error && (
+              <ErrorContainer>
+                <Text variant="error">Error: {error}</Text>
+              </ErrorContainer>
+            )}
+          </>
         )}
       </AccountContainer>
       <Spacer size="large" />
