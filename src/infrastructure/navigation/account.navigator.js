82c20b90dbb7b605484c1d34e5539b73c9be555f
Bertrand <mutanganabertrand@gmail.com>
2022-12-07 10:52:25 +0200

    Create screens

--- src/infrastructure/navigation/account.navigator.js
+++ src/infrastructure/navigation/account.navigator.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { View, Text } from 'react-native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import AccountScreen from '../../features/account/screens/account.screen'
+import LoginScreen from '../../features/account/screens/login.screen'
+import RegisterScreen from '../../features/account/screens/register.screen'
 
 const Stack = createNativeStackNavigator()
 
@@ -11,22 +13,9 @@ const AccountNavigator = () => {
         headerShown: false,
       }}
     >
-      <Stack.Screen
-        name="Main"
-        component={() => (
-          <View>
-            <Text>Account Screen</Text>
-          </View>
-        )}
-      />
-      <Stack.Screen
-        name="Login"
-        component={() => (
-          <View>
-            <Text>Login Screen</Text>
-          </View>
-        )}
-      />
+      <Stack.Screen name="Main" component={AccountScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
     </Stack.Navigator>
   )
 }
