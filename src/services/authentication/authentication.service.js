6b1a311aa5e612545df5817d6b8ce0e7afda0588
Bertrand <mutanganabertrand@gmail.com>
2022-12-07 15:32:25 +0200

    Fix

--- src/services/authentication/authentication.service.js
+++ src/services/authentication/authentication.service.js
@@ -1,10 +1,12 @@
-import { signInWithEmailAndPassword } from 'firebase/auth'
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth'
 import { auth } from '../../../firebaseConfig'
 
 export const login = async (email, password) => {
   try {
     const { user } = await signInWithEmailAndPassword(auth, email, password)
-    console.log('user', user)
     return user
   } catch (error) {
     throw error
